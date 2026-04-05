364a3a93a419751dfab51bcc7ef1767f50c11a22
Dev-WilliamThug <williamngassam2910@gmail.com>
2026-04-05 22:33:00 +0100

    Intégration de la base de données externes avec postgresql
    J'ai connecté l'application avec une BD externe appelé gestionEquipement avec Postgresql.

--- config/settings.py
+++ config/settings.py
@@ -81,8 +81,12 @@ LOGOUT_REDIRECT_URL = "users:login"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gestionEquipement',
+        'USER': 'postgres',
+        'PASSWORD': 'pass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
